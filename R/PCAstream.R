7f80b6ec57566e400d6c1116b84fcc2d2bbb9c65
Zilong-Li <zimusen94@gmail.com>
2025-04-14 22:20:25 +0200

    update winSVDstream

--- R/PCAstream.R
+++ R/PCAstream.R
@@ -59,42 +59,46 @@
 #' @importFrom methods slot
 #' @importFrom progress progress_bar
 #' @export
-winSVDstream <- function(gds, k, p = 7, s = 10, B = 64, quiet = FALSE) {
+winSVDstream <- function(gds, k, p = 7, s = 10, B = 64, threads = 16, quiet = FALSE) {
 
   stopifnot(is(gds, "GenomicDataStream"))  
-    
-  gds <- reinitializeStream(gds)
+  chunkSizes <- summaryChunks(gds)
   N <- slot(gds, "nsamples")
-  chunkSizes <- countChunks(gds)
-  M <- sum(chunkSizes)
+  M <- sum(chunkSizes$counts)
 
-  # B must be a even, and larger than length(chunkSizes)
-  b <- ceiling(log2(length(chunkSizes)))
-  B <- min(B, b + b %% 2)
-
+  # M must be large enough, otherwise winSVD is not suggested
+  stopifnot(M > B^2)
+  # B must be a even
   stopifnot(B %% 2 == 0)
+  # nchunks must be larger than B * threads
+  stopifnot(nrow(chunkSizes) > B * threads)
+
   p <- max(c(p, log2(B)+1))  
   
+  ## each bucket has the index of permuted blocks for each thread
+  idx_per_thread <- splitPermuteChunksByThreads(chunkSizes, threads, B)
+
   if( ! quiet ){
-    cat("B: ", B, "\n")
-    cat("p: ", p, "\n")
+    cat("p: ", p, "\t")
+    cat("nchunks: ", nrow(chunkSizes), "\t")
+    cat("B: ", B, "\t")
+    cat("M: ", M, "\t")
+    cat("N: ", N, "\t")
+    cat("T: ", length(idx_per_thread), "\n")
   }
 
   L <- min(k + s, M)
   Omega <- matrix(rnorm(N*L), nrow=N, ncol=L) # N x L
-  H <-  0  ## N x L
-  H1 <-  0  ## N x L
-  H2 <-  0  ## N x L
+  H <- matrix(0,ncol=L,nrow=N) ## N x L
+  H1 <- matrix(0,ncol=L,nrow=N) ## N x L
+  H2 <- matrix(0,ncol=L,nrow=N) ## N x L
   G <- matrix(NA, nrow=M, ncol=L) ## M x L
 
   switch <- TRUE
-
-  ## each bucket has the index of blocks
-  buckets <- split_into_buckets(seq(length(chunkSizes)), B)
   band <- 2  ## initial number of buckets as a band
 
   # indeces were chunks are placed in R
-  idx = c(1, 1 + cumsum(chunkSizes))
+  ## idx = c(1, 1 + cumsum(chunkSizes))
 
   if (!quiet) {
     pb <- progress_bar$new(
@@ -109,28 +113,35 @@ winSVDstream <- function(gds, k, p = 7, s = 10, B = 64, quiet = FALSE) {
       H2[] <-  0  ## N x L
     }
 
-    gds <- reinitializeStream(gds)
-    
+    ss <- initializeMultiStream(gds, idx_per_thread, chunkSizes)
+
+    bi <- 1
     for(b in seq(B)) {
 
       if (!quiet) pb$tick()
 
       j <- j + 1
-      for(w in buckets[[b]]) {
+      nloops <- nrow(idx_per_thread[[1]]) ## the minimum number of loops
+      
+      for(n in seq(nloops)) {
+        Ab <- getChunksParallel(ss[1:threads], threads)
+        idx <- unlist(sapply(idx_per_thread[1:threads], function(x) x[n, b]))
 
-        dat <- getNextChunk(gds)
-        
-        bi <- idx[w]
-        bj <- idx[w+1] - 1
+        if(length(ss) > threads && n <= nrow(idx_per_thread[[threads+1]]) && !is.na(idx_per_thread[[threads+1]][n,b])) {
+          dat <- getNextChunk(ss[[length(ss)]])
+          standardize_in_place( dat$X )
+          Ab <- cbind( Ab, dat$X )
+          idx <- c(idx,  idx_per_thread[[threads+1]][n,b])
+        }
 
-        # normalize genotype matrix directly
-        # standardize columns in place
-        # Ab <- scale(dat$X)
-        Ab <- dat$X
-        standardize_in_place( Ab )
-
+        bj <- bi + ncol(Ab) - 1
+        if(bj > M) {
+          print(Ab)
+          print(c(i, bi, bj, ncol(Ab)))
+        }
         Gb <- crossprod(Ab, Omega)
         G[seq(bi,bj),] <- Gb
+        bi <- bj + 1
 
         if(j <= band / 2) {
           H1 <- H1 + Ab %*% Gb
@@ -159,6 +170,7 @@ winSVDstream <- function(gds, k, p = 7, s = 10, B = 64, quiet = FALSE) {
         H2[] <- 0
       }
     }
+    stopifnot(bj != M)
     band <- min(B,round(band * 2))
   }
 
@@ -171,7 +183,17 @@ winSVDstream <- function(gds, k, p = 7, s = 10, B = 64, quiet = FALSE) {
   getUSV_t(H, G, k)
 }
 
-
+getUSV_t <- function(H,G,k){
+  r1 <- qr(G)
+  Q <- qr.Q(r1)
+  r2 <- qr(Q)
+  Rtilt<-  qr.R(r1)
+  Rhat <-  qr.R(r2)
+  R <- Rhat %*% Rtilt
+  B <- qr.solve(t(R), t(H))
+  dcmp <- svd(B, nu = k, nv = k)
+  list(d = dcmp$d[1:k], u = dcmp$v, v = G %*% dcmp$u)
+}
 
 split_into_buckets <- function(sequence, B) {
   n <- length(sequence)
@@ -191,16 +213,103 @@ split_into_buckets <- function(sequence, B) {
   split(sequence, bucket_indices)
 }
 
+#' Get array of chunk sizes
+#' 
+#' @examples
+#' file <- system.file("extdata", "test.vcf.gz", package = "GenomicDataStream")
+#'
+#' # initialize
+#' obj <- GenomicDataStream(file, "DS", chunkSize = 3, initialize = TRUE)
+#' summaryChunks(obj)
+#' 
+#' @export
+summaryChunks <- function(x){
+  counts <- c()
+  chunks <- data.frame(matrix(ncol=3,nrow=0), stringsAsFactors = F)
+  # count number of variants
+  x <- reinitializeStream(x)
+  while(1){
+    dat <- getNextChunk(x)
+    chunks = rbind(chunks, list(dat$info[1,1], dat$info[1, 2], dat$info[nrow(dat$info),2]+1, ncol(dat$X)))
+    if (atEndOfStream(x)) break
+  }
 
-getUSV_t <- function(H,G,k){
-  r1 <- qr(G)
-  Q <- qr.Q(r1)
-  r2 <- qr(Q)
-  Rtilt<-  qr.R(r1)
-  Rhat <-  qr.R(r2)
-  R <- Rhat %*% Rtilt
-  B <- qr.solve(t(R), t(H))
-  dcmp <- svd(B, nu = k, nv = k)
-  list(d = dcmp$d[1:k], u = dcmp$v, v = G %*% dcmp$u)
+  colnames(chunks) <- c("chrom", "start", "end", "counts")
+  chunks$region <- paste0(chunks$chrom, ":", chunks$start, "-", chunks$end)
+  chunks[,c("region", "counts")]
 }
+
+getChunksParallel <- function(gds, ncores) {
+  r <- parallel::mclapply(gds, function(x) {
+    dat <- getNextChunk(x)
+    standardize_in_place( dat$X )
+    dat$X
+  }, mc.cores = ncores)
+  do.call(cbind, r)
+}
+
+initializeMultiStream <- function(x, idx_per_thread, chunks) {
+  lapply(idx_per_thread, function(idx) {
+    idx <- as.vector(idx)
+    region <- paste(chunks[idx[!is.na(idx)], "region"], collapse = ",")
+    GenomicDataStream(
+      file = x@file,
+      field = x@field,
+      region = region,
+      samples = x@samples,
+      minVariance = x@minVariance,
+      chunkSize = x@chunkSize,
+      missingToMean = x@missingToMean,
+      initialize = TRUE
+    )
+  })
+}
+
+
+## regions <- summaryChunks(obj)
+## B <- 64
+## M <- 6410
+## T <- 16
+
+## make a matrix : threads x (nloops x bucket)
+splitPermuteChunksByThreads <- function(chunks, threads = 16, B = 64) {
+  M <- nrow(chunks)
+  idx <- c(rep(1:B, times = M/B), 1:(M %% B))
+  chunks_per_window <- split(seq(M), idx)
+  T <- threads
+
+  ## w <- chunks_per_window[[1]]
+  ## idx <- c(rep(1:T, each = length(w)/T), sample(1:T,length(w) %% T))
+  ## chunks_per_thread <- split(w, idx)
+  ret <- lapply(1:B, function(b) {
+    w <- chunks_per_window[[b]]
+    nloops <- length(w)/T
+    ## remain <- sample(1:T,length(w) %% T)
+    ## idx <- c(rep(1:T, each = nloops), remain)
+    idx <- rep(1:T, each = nloops)
+    chunks_per_thread <- split(w[1:length(idx)], idx)
+    chunks_per_thread[[T+1]] <- w[(length(idx)+1):length(w)]
+    chunks_per_thread
+  })
+
+
+  idx_per_thread <- lapply(1:T, function(t) unlist(sapply(ret, function(x) x[[t]])))
+
+  remain <- unlist(sapply(ret, function(x) x[[T+1]] ) )
+  if(length(remain)>0) {
+    remain <- split_into_buckets(remain, B)
+    remain_idx <- t(do.call(rbind, lapply(remain, function(x) {
+      length(x) <- max(lengths(remain))  # Set lengths to maximum length
+      x
+    })))
+    idx_per_thread <- c(idx_per_thread, list(remain_idx))
+  }
+
+  idx_per_thread
+
+}
+
+
+
+
 
