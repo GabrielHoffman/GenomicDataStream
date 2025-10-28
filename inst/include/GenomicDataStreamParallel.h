f5b2cdee10abc5eb6a62db2313443586db9bd98c
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2025-10-27 20:48:57 -0400

    message

--- inst/include/GenomicDataStreamParallel.h
+++ inst/include/GenomicDataStreamParallel.h
@@ -49,8 +49,7 @@ class GenomicDataStreamParallel {
     // });
 
     for(int i=0; i<numThreads; i++){
-      // readers[i] = gds::createFileView(param);
-      readers[i] = make_shared<vcfstream>( param );
+      readers[i] = gds::createFileView(param);
     }
 
 
