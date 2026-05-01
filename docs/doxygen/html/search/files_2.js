72cffd0ad68dec6c17f664826832d243e6f4cfdf
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2026-05-01 11:08:54 -0400

    update docs

--- docs/doxygen/html/search/files_2.js
+++ docs/doxygen/html/search/files_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['datainfo_2eh_0',['DataInfo.h',['../_data_info_8h.html',1,'']]],
-  ['datatable_2eh_1',['DataTable.h',['../_data_table_8h.html',1,'']]],
-  ['delayedstream_2eh_2',['DelayedStream.h',['../_delayed_stream_8h.html',1,'']]]
+  ['dashsvd_2eh_0',['dashSVD.h',['../dash_s_v_d_8h.html',1,'']]],
+  ['datainfo_2eh_1',['DataInfo.h',['../_data_info_8h.html',1,'']]],
+  ['datatable_2eh_2',['DataTable.h',['../_data_table_8h.html',1,'']]],
+  ['delayedstream_2eh_3',['DelayedStream.h',['../_delayed_stream_8h.html',1,'']]]
 ];
